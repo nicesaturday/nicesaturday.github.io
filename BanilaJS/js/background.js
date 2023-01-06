1061a24ee2ec766c4806085a3568c3e87de746eb
Mansik <68882378+nicesaturday@users.noreply.github.com>
2023-01-07 00:28:44 +0900

    Add files via upload

---
+++ BanilaJS/js/background.js
@@ -0,0 +1,16 @@
+const images = ["0.jpg","1.jpg","2.jpg"];
+
+
+const chosenImage = images[Math.floor(Math.random() * images.length)];
+
+const bgimage = document.createElement("img");
+
+bgimage.id = "allImage";
+
+
+bgimage.src = `img/${chosenImage}`;
+
+
+
+
+document.body.appendChild(bgimage);
